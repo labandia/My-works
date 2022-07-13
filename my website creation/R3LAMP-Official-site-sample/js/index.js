890042979a5b552ab855c7d995841a34e6f81364
labandia <65441114+labandia@users.noreply.github.com>
2022-07-13 13:06:12 +0800

    add classroom and movie

--- my website creation/R3LAMP-Official-site-sample/js/index.js
+++ my website creation/R3LAMP-Official-site-sample/js/index.js
@@ -1,91 +1,113 @@
 /*=============== SHOW MENU ===============*/
-const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close')
+const navMenu = document.getElementById("nav-menu"),
+   navToggle = document.getElementById("nav-toggle"),
+   navClose = document.getElementById("nav-close");
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-if(navToggle){
-    navToggle.addEventListener('click', () =>{
-        navMenu.classList.add('show-menu')
-    })
+if (navToggle) {
+   navToggle.addEventListener("click", () => {
+      navMenu.classList.add("show-menu");
+   });
 }
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-if(navClose){
-    navClose.addEventListener('click', () =>{
-        navMenu.classList.remove('show-menu')
-    })
+if (navClose) {
+   navClose.addEventListener("click", () => {
+      navMenu.classList.remove("show-menu");
+   });
 }
-
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
 
-const nav = document.querySelector('header');
+const nav = document.querySelector("header");
 
-window.addEventListener('scroll', ()=>{
-    if(window.scrollY >= 100){
-        nav.classList.add('active_nav');
-    } else{
-        nav.classList.remove('active_nav');
-    }
-})
-
+window.addEventListener("scroll", () => {
+   if (window.scrollY >= 100) {
+      nav.classList.add("active_nav");
+   } else {
+      nav.classList.remove("active_nav");
+   }
+});
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
 
-const tabs = document.querySelectorAll('.instruct__toggle'), 
-     contents =  document.querySelectorAll('.instruct__content');
+const tabs = document.querySelectorAll(".instruct__toggle"),
+   contents = document.querySelectorAll(".instruct__content");
 
-tabs.forEach((tab, index)=>{
-    tab.addEventListener('click', () => {
-        contents.forEach((content) => {
-            content.classList.remove('is-active');
-        });
-        tabs.forEach((tab) => {
-            tab.classList.remove('is-active');
-        });
+tabs.forEach((tab, index) => {
+   tab.addEventListener("click", () => {
+      contents.forEach((content) => {
+         content.classList.remove("is-active");
+      });
+      tabs.forEach((tab) => {
+         tab.classList.remove("is-active");
+      });
 
-        contents[index].classList.add('is-active');
-        tabs[index].classList.add('is-active');
-    })
-})
+      contents[index].classList.add("is-active");
+      tabs[index].classList.add("is-active");
+   });
+});
 
+const sections = document.querySelectorAll("section[id]");
 
+function scrollActive() {
+   const scrollY = window.pageYOffset;
 
-const sections = document.querySelectorAll('section[id]')
+   sections.forEach((current) => {
+      const sectionHeight = current.offsetHeight,
+         sectionTop = current.offsetTop - 58,
+         sectionId = current.getAttribute("id");
 
-function scrollActive(){
-    const scrollY = window.pageYOffset
-
-    sections.forEach(current =>{
-        const sectionHeight = current.offsetHeight,
-              sectionTop = current.offsetTop - 58,
-              sectionId = current.getAttribute('id')
-
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
-            // document.querySelector('.active_nav.nav__link').classList.add('active');
-        }else{
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active');
-        }
-    })
+      if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+         document
+            .querySelector(".nav__menu a[href*=" + sectionId + "]")
+            .classList.add("active");
+         // document.querySelector('.active_nav.nav__link').classList.add('active');
+      } else {
+         document
+            .querySelector(".nav__menu a[href*=" + sectionId + "]")
+            .classList.remove("active");
+      }
+   });
 }
-window.addEventListener('scroll', scrollActive);
-
+window.addEventListener("scroll", scrollActive);
 
 const sr = ScrollReveal({
-    origin: 'top',
-    distance: '60px',
-    duration: 2500,
-    delay: 400
+   origin: "top",
+   distance: "60px",
+   duration: 2500,
+   delay: 400,
 });
 
 sr.reveal(`.home__data`);
-sr.reveal(`.home__imgcontainer`, {delay: 500});
-sr.reveal(`.faq__cons`, {interval: 100});
-sr.reveal(`.features__imgcon, .download__title`, {origin: 'left'});
-sr.reveal(`.features__data, .download__details`, {origin: 'right'});
+sr.reveal(`.home__imgcontainer`, { delay: 500 });
+sr.reveal(`.faq__cons`, { interval: 100 });
+sr.reveal(`.features__imgcon, .download__title`, { origin: "left" });
+sr.reveal(`.features__data, .download__details`, { origin: "right" });
+
+const btn2 = document.getElementsByClassName("navbottom");
+
+windows.onscroll = function () {
+   scrollFunction();
+};
+
+function scrollFunction() {
+   if (
+      document.body.scrollTop > 300 ||
+      document.documentElement.scrollTop > 300
+   ) {
+      btn2.style.display = "block";
+   } else {
+      btn2.style.display = "none";
+   }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+   document.body.scrollTop = 0;
+   document.documentElement.scrollTop = 0;
+}
